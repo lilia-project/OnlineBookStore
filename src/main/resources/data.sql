1729a30f3effbabab95385b4b6f83a1820266856
Lilia <trek0952056799@gmail.com>
2023-08-20 16:30:23 +0200

    Create data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,10 @@
 insert into public.author(id, name, surname)
 	VALUES (1,'Владимир', 'Набоков'),
 	        (2,'Эрнест', 'Хемингуэй'),
-	        (3,'Андрей', 'Платонов');
+	        (3,'Джордж', 'Оруэлл'),
+	        (4,'Михаил', 'Шолохов'),
+	        (5,'Альбер', 'Камю'),
+	        (6,'Джером', 'Сэлинджер'),
+	        (7,'Андрей', 'Платонов');
 
 select setval((select pg_get_serial_sequence('public.author', 'id')), (select max(id) from public.author));
