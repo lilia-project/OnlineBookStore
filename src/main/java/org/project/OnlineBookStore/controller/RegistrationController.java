e89d9f363cb71b3d94e01e1b9902c09f1c9a01bb
Lilia <trek0952056799@gmail.com>
2023-08-24 21:49:32 +0200

    Create registration

---
+++ src/main/java/org/project/OnlineBookStore/controller/RegistrationController.java
@@ -0,0 +1,57 @@
+package org.project.OnlineBookStore.controller;
+
+import org.project.OnlineBookStore.entity.Role;
+import org.project.OnlineBookStore.entity.User;
+import org.project.OnlineBookStore.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+
+@Controller
+public class RegistrationController {
+    private final UserService userService;
+
+    @Autowired
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/registration")
+    public String registrationPost(@RequestParam String username,
+                                   @RequestParam String password,
+                                   @RequestParam String email) {
+        final User user = new User();
+        user.setPassword(password);
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setRoles(Collections.singleton(Role.USER));
+        userService.addUser(user, false);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            request.getSession().invalidate();
+        }
+        return "redirect:/";
+    }
+}
