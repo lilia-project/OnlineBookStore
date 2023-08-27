695f03658884d66eb0c213159557b5ad618bd82d
Lilia <trek0952056799@gmail.com>
2023-08-27 20:25:58 +0200

    Add error pages

---
+++ src/main/java/org/project/OnlineBookStore/exception/GlobalExceptionHandler.java
@@ -0,0 +1,56 @@
+package org.project.OnlineBookStore.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    protected ModelAndView handleRuntime(HttpServletRequest req, Exception ex) {
+        log.error("Request: " + req.getRequestURL() + " raised " + ex);
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", ex);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName("error/error500");
+
+        return mav;
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    protected ModelAndView handleNotFound(HttpServletRequest req, Exception ex) {
+        log.warn("Request: " + req.getRequestURL() + " raised " + ex);
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", ex);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName("error");
+
+        return mav;
+    }
+
+    @ExceptionHandler(BookOutOfStockException.class)
+    @ResponseStatus(value = HttpStatus.CONFLICT)
+    protected ModelAndView handleConflict(HttpServletRequest req, Exception ex) {
+        log.warn("Request: " + req.getRequestURL() + " raised " + ex);
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", ex);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName("error/error409");
+
+        return mav;
+    }
+
+}
---
+++ src/main/java/org/project/OnlineBookStore/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package org.project.OnlineBookStore.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
